51832a8814943c45d61b8108905d70fbebde6b75
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-05 18:48:30 +0530

    Added:Randomly win or loose

--- GamblingGame.java
+++ GamblingGame.java
@@ -4,6 +4,14 @@ public class GamblingGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling simulation program on master branch");
-
+        int win,loose;
+        int game = (int)(Math.random() * 2);
+        if (game == 1) {
+            everyDayStake+=everyGameBet;
+            System.out.println("Win the game :The amount is = "+everyDayStake);
+        } else {
+            everyDayStake-=everyGameBet;
+            System.out.println("Loose the game :The amount is ="+everyDayStake);
+        }
     }
 }
