ae7eee28f93200944686267177ce20e8759b0bcf
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-05 18:34:46 +0530

    Added:Declare given data

---
+++ GamblingGame.java
@@ -0,0 +1,9 @@
+public class GamblingGame {
+    public static final int everyGameBet = 1;
+    public static int everyDayStake = 100;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambling simulation program on master branch");
+
+    }
+}
